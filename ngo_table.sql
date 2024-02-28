8205f4fbf070db3b97f8ec2dd7f15933ff0547ee
Soumili <119061280+soumili-03@users.noreply.github.com>
2024-02-28 16:23:17 +0530

    ngo table

---
+++ ngo_table.sql
@@ -0,0 +1,12 @@
+#create database NGOlogs;
+use NGOlogs;
+Create table
+NGO(ngo_ID int primary key not null,
+	website_link varchar(30), 
+    city varchar(30), 
+    cause varchar(30),
+    ngo_email varchar(30));
+
+    
+    
+    

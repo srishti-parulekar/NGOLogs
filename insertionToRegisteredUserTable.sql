8f09183b0e1080d1e8af45ccb9ed33a02ecf78e1
TanV-04 <119609232+TanV-04@users.noreply.github.com>
2024-03-06 13:36:11 +0530

    Add files via upload

---
+++ insertionToRegisteredUserTable.sql
@@ -0,0 +1,6 @@
+
+insert into registered_user(registered_userID, registered_username, username, user_password, gender, phone, user_email) values (2024007, "Fathima Malik", "FM$78", "pass120", "F", 9912345678, "fathima120@gmail.com"), (2024008, "Yusuf Abbas", "YA%68", "pass130", "M", 9912345679, "yusuf130@gmail.com");
+
+
+# my command line client crashed and i lost all the code for the previously inserted queries.
+# here's two more to get you started <3

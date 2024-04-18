2b9cd0e3ebf079bb1694ff9d14d1c7c3bbb58c01
TanV-04 <119609232+TanV-04@users.noreply.github.com>
2024-04-18 22:06:14 +0530

    Add files via upload

---
+++ Donortable.sql
@@ -0,0 +1,5 @@
+-- use ngologs;
+
+-- create table donor(donorID int primary key auto_increment, ngo_id int, user_ID int, amountContributed int, timeOfDonation TIMESTAMP DEFAULT CURRENT_TIMESTAMP, foreign key (ngo_id) references ngo(ngoID), foreign key (user_Id) references person(userID));
+
+-- insert into donor(donorid, ngo_id, user_id, amountContributed) values(1, 1424001, 2024001, 1500);

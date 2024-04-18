7ab7606c767330ae68d4ce01521ae8ef5c1d7231
TanV-04 <119609232+TanV-04@users.noreply.github.com>
2024-04-18 21:42:30 +0530

    Delete insertionToNGOTable.sql

--- insertionToNGOTable.sql
+++
@@ -1,5 +0,0 @@
-insert into ngo values(1424001, "www.sendHelp.com", "Pune", "Education", "sendHelpFrontDesk@gmail.com", 9989123456);
-insert into ngo values(1424002, "www.narayanFoundation.com", "Hyderabad", "Health", "narayanFoundationSecretary@gmail.com", 9989123457);
-insert into ngo values(1424003, "www.aaradhyaFoundation.com", "Mumbai", "Poverty", "aaradhyaFoundationHR@gmail.com", 9989123458);
-insert into ngo values(1424004, "www.endHunger.com", "Vadodara", "Food", "endThisOutrageousHunger@gmail.com", 9989123459);
-insert into ngo values(1424005, "www.stopThisAbuse.com", "Chennai", "Human Trafficking", "stopAbuse@gmail.com", 9989123478);

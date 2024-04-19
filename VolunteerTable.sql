88c30aa96330b679379f42f4768ba824d443daad
TanV-04 <119609232+TanV-04@users.noreply.github.com>
2024-04-19 19:46:14 +0530

    Add files via upload

---
+++ VolunteerTable.sql
@@ -0,0 +1,13 @@
+-- use ngologs;
+
+-- CREATE TABLE Volunteer (
+--     volunteerId INT PRIMARY KEY AUTO_INCREMENT,
+--     user_id INT,
+--     ngoID INT,
+--     skills VARCHAR(255),
+--     interests VARCHAR(255),
+--     availability ENUM('Morning', 'Afternoon', 'Evening'),
+--     foreign key (ngoID) references ngo(ngoid)
+-- );
+
+-- insert into volunteer values(1, 2024002, 1424002, "Problem Solving, Tech Background, Team Leader", "Singing, Boxing, Teaching", "Evening");

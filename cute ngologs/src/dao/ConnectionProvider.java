760f9d83eec3497ad5c2e72afe2c087ef6d974c9
Soumili <119061280+soumili-03@users.noreply.github.com>
2024-04-28 18:52:56 +0530

    Update ConnectionProvider.java

--- cute ngologs/src/dao/ConnectionProvider.java
+++ cute ngologs/src/dao/ConnectionProvider.java
@@ -13,7 +13,7 @@ public class ConnectionProvider {
     public static Connection getCon(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ngologs2","root","09October*");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ngologs2","root","");
             return conn;
         }
     
